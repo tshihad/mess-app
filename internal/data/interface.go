66144805636fd9e45defcd11adf6c87de2c1460e
shihad <tshihad9@gmail.com>
2019-05-14 16:46:23 +0530

    added response success and fail

--- internal/data/interface.go
+++ internal/data/interface.go
@@ -1,8 +1,17 @@
 package data
 
+import "net/http"
+
 // Repo - Data repository
 type Repo interface {
 }
 
+// Cache := TODO
 type Cache interface {
+}
+
+// APIRespose defined Success and Fail responses
+type APIRespose interface {
+	Success(http.ResponseWriter, int, interface{})
+	Fail(http.ResponseWriter, int, string, int)
 }
