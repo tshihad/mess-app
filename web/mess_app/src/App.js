71a57fde4e03e43fc57835737ebfffc14323c93f
adarsh97vtk <adarsh97vtk@gmail.com>
2019-05-17 11:46:00 +0530

    top and bottom bar

--- web/mess_app/src/App.js
+++ web/mess_app/src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import HomeIcon from '@material-ui/icons/Home';
+import Group from '@material-ui/icons/Group';
+import Assignment from '@material-ui/icons/Assignment';
+import SwapHoriz from '@material-ui/icons/SwapHoriz';
+import Restaurant from '@material-ui/icons/RestaurantMenu';
+import Typography from '@material-ui/core/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    },
+    bottomBar: {
+        top: 'auto',
+        bottom: 0,
+    },
+    toolbar: {
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+});
+
+function BottomAppBar(props) {
+    const { classes } = props;
+    return (
+        <Fragment>
+            <div className={classes.root}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" color="inherit" className={classes.grow}>
+                            Mess App
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+            </div>
+            <CssBaseline />
+            <AppBar position="fixed" color="primary" className={classes.bottomBar}>
+                <Toolbar className={classes.toolbar}>
+                    <IconButton color="inherit" >
+                        <HomeIcon />
+                    </IconButton>
+                    <IconButton color="inherit" >
+                        <Group />
+                    </IconButton>
+                    <IconButton color="inherit" >
+                        <Assignment />
+                    </IconButton>
+                    <IconButton color="inherit" >
+                        <Restaurant />
+                    </IconButton>
+                    <IconButton color="inherit" >
+                        <SwapHoriz />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+        </Fragment>
+    );
 }
 
-export default App;
+BottomAppBar.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(BottomAppBar);
